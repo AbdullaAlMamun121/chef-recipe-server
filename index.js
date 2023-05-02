e28e07fb40d6264d988fc6fd7cd63db8f86450b9
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-05-02 23:04:25 +0600

    edit json file and add some items

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefInfo = require('./data/chef-info.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("chef server is running")
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chefInfo);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const chefFound = chefInfo.find(n =>n.id == id);
+    res.send(chefFound);
 })
 
 app.listen(port, () => {
